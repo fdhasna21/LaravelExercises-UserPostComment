88557250657af3ea2b3465e14ad545d348e05184
fdhasna21 <fernanda.daymara.hasna@gmail.com>
2021-05-20 08:20:11 +0700

    Add algorithm for save data to database using Model

--- app/Http/Controllers/API/v1/RegistrationController.php
+++ app/Http/Controllers/API/v1/RegistrationController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\API\v1;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 
 class RegistrationController extends Controller
@@ -16,21 +17,31 @@ class RegistrationController extends Controller
             'email' => 'required|email|unique:users,email',
             'password' => 'required|confirmed' //will automatically check with password_confirmation
         ]);
-
-        $name = $request->name;
-        $email = $request->email;
-        $password = Hash::make($request->password);
-
-        $data = array('name'=>$name,
-                    'email'=>$email,
-                    'password'=>$password,
-                    'created_at'=>now(),
-                    'updated_at'=>now(),
-                    'email_verified_at'=>now(),
-                    'remember_token'=>Str::random(10));
         //email_verified_at and remember_token should have better algorithm
 
-        DB::table('users')->insert($data);
+        //If using insert database :
+        // $name = $request->name;
+        // $email = $request->email;
+        // $password = Hash::make($request->password);
+        // $data = array('name'=>$name,
+        //             'email'=>$email,
+        //             'password'=>$password,
+        //             'created_at'=>now(),
+        //             'updated_at'=>now(),
+        //             'email_verified_at'=>now(),
+        //             'remember_token'=>Str::random(10));
+        // DB::table('users')->insert($data);
+
+
+        //If save data using Model :
+        $user = new User();
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->password = Hash::make($request->password);
+        $user->email_verified_at= now();
+        $user->remember_token= Str::random(10);
+        $user->save();
+        
         return response(['success'=> true], 200);
     }
 }
